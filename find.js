73a34ed654ee90610d446c791e812cb47d914c0b
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2023-12-04 13:12:02 +0530

    find.js

---
+++ find.js
@@ -0,0 +1,11 @@
+//taking number as user input:
+let num=parseFloat(prompt("Enter the number:"));
+function finds(num){
+if(num%2===0){
+console.log("the given number is even");
+}
+else{
+console.log("the given number is odd");
+}
+}
+finds(num);
